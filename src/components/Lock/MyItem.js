2be64d3f16a09ccb58d151c67cc11366ebd03637
kausarahamed <galibhasan517@gmail.com>
2022-05-06 16:04:41 +0600

    add Item

--- src/components/Lock/MyItem.js
+++ src/components/Lock/MyItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Product from "../Home/Product";
 
 const MyItem = () => {
   return <div>jsdhfgjsdhb</div>;
--- src/components/update/Update.js
+++ src/components/update/Update.js
@@ -9,10 +9,55 @@ const Update = () => {
       .then((response) => response.json())
       .then((data) => setproduct(data));
   }, [id]);
-  console.log(product);
+  const { name, email, image, description, supplier, quantity, price } =
+    product;
+
   return (
     <div>
-      <h1>urshgdjh{id}</h1>
+      <div className="md:flex  ">
+        <div className=" md:w-1/2">
+          <img src={image} alt="" />
+        </div>
+        <div className="md:w-1/2 mt-16">
+          <h1 className="text-4xl font-serif italic text-center text-blue-500 font-bold pt-1">
+            {name}
+          </h1>
+          <h1 className="text-xl mb-2">
+            <span className="text-2xl font-bold">Email:</span> {email}
+          </h1>
+          <h1 className="text-xl mb-2">
+            <span className="text-2xl font-bold">Description:</span>{" "}
+            {description}
+          </h1>
+          <h1 className="text-xl mb-2">
+            <span className="text-2xl font-bold">Supplier:</span> {supplier}
+          </h1>
+          <h1 className="text-xl mb-2">
+            <span className="text-2xl font-bold">Quantity</span>: {quantity}
+          </h1>
+          <h1 className="text-xl mb-2">
+            <span className="text-2xl font-bold">Price:</span> {price}$
+          </h1>
+        </div>
+      </div>
+      <div className="flex justify-evenly mb-10">
+        <button className="cursor-pointer border-2 bg-orange-400 p-2 rounded-lg">
+          Deliver
+        </button>
+        <form>
+          <input
+            className="p-2"
+            type="number"
+            name="quantity"
+            placeholder="Add Quantity"
+          />
+          <input
+            className="cursor-pointer border-2 bg-orange-400 p-2 rounded-lg "
+            type="submit"
+            value="Add Quantity"
+          />
+        </form>
+      </div>
     </div>
   );
 };
