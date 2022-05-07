d44829466a47253edca52221d34d4e72c39764dd
kausarahamed <galibhasan517@gmail.com>
2022-05-07 20:55:52 +0600

    my item done

--- src/components/Lock/MyItem.js
+++ src/components/Lock/MyItem.js
@@ -1,8 +1,21 @@
-import React from "react";
-import Product from "../Home/Product";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../firebase.init";
 
 const MyItem = () => {
-  return <div>jsdhfgjsdhb</div>;
+  const [user] = useAuthState(auth);
+  const email = user.email;
+  const [product, setproduct] = useState([]);
+  useEffect(() => {
+    fetch(`https://intense-stream-06695.herokuapp.com/items?email=${email}`)
+      .then((response) => response.json())
+      .then((data) => setproduct(data));
+  }, []);
+  return (
+    <div>
+      <p>{product.length}</p>
+    </div>
+  );
 };
 
 export default MyItem;
